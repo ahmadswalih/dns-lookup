835f815fb59aaf6b7e9355d15364ca706f2055b9
ahmadswalih <ahmadswalih777@gmail.com>
2023-11-08 08:10:21 +0530

    updated route url

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 import { checkDomain } from "../controllers/DomainController.js";
 
 // Define the route for checking domain information (including DNS records, expiration date, and owner info)
-router.get("/:domain", checkDomain);
+router.get("/domain/:domain", checkDomain);
 
 export default router;
